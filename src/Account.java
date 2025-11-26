3d1fc5587e35248eb97768643a1b11c8c0b03f27
Brandon Hamilton <bham2015@gmail.com>
2025-11-25 18:07:24 -0600

    Started bank project.

---
+++ src/Account.java
@@ -0,0 +1,16 @@
+public class Account {
+
+    public double AccountBalance = 0.0;
+
+    public void deposit(double DepositValue) {
+        AccountBalance = AccountBalance + DepositValue;
+    }
+
+    public void withdraw(double WithdrawValue) {
+        if (AccountBalance <= 0) {
+            System.out.println("You cannot withdraw from this account you faka");
+        } else {
+            AccountBalance = AccountBalance - WithdrawValue;
+        }
+    }
+}
